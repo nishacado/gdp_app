1fafd27fa825bc6128a70882a5cdc9333d96f9d3
Nishanthi <nishanthirajakumar@gmail.com>
2022-12-03 15:29:35 +0800

    updated app

--- App/app.py
+++ App/app.py
@@ -1,37 +1,33 @@
+#Libaray Import 
+
 import streamlit as st
 import pathlib
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
 
-#-------------
-#layout design 
-#-------------
+#Title 
 
-st.title('Country GDP Estimation Tool')
-st.write('''
-         This app will estimate the GDP per capita for a country, given some 
-         attributes for that specific country as input.
-         
-         Please fill in the attributes below, then hit the GDP Estimate button
-         to get the estimate. 
-         ''')
+st.title('GDP Estimation Tool')
+st.write(''' This app is used to estimate the GDP based on the input value of the user. ''')
 
-st.header('Input Attributes')
+#Designing of the Variable
+
+st.header('User Input')
 att_popl = st.number_input('Population (Example: 7000000)', min_value=1e4, max_value=2e9, value=2e7)
-att_area = st.slider('Area (sq. Km)', min_value= 2.0, max_value= 17e6, value=6e5, step=1e4)
-att_dens = st.slider('Population Density (per sq. mile)', min_value= 0, max_value= 12000, value=400, step=10)
-att_cost = st.slider('Coastline/Area Ratio', min_value= 0, max_value= 800, value=30, step=10)
-att_migr = st.slider('Annual Net Migration (migrant(s)/1,000 population)', min_value= -20, max_value= 25, value=0, step=2) 
-att_mort = st.slider('Infant mortality (per 1000 births)', min_value= 0, max_value=195, value=40, step=10)
-att_litr = st.slider('Population literacy Percentage', min_value= 0, max_value= 100, value=80, step=5)
-att_phon = st.slider('Phones per 1000', min_value= 0, max_value= 1000, value=250, step=25)
-att_arab = st.slider('Arable Land (%)', min_value= 0, max_value= 100, value=25, step=2)
-att_crop = st.slider('Crops Land (%)', min_value= 0, max_value= 100, value=5, step=2)
-att_othr = st.slider('Other Land (%)', min_value= 0, max_value= 100, value=70, step=2)
+att_area = st.number_input('Area (sq. Km)', min_value= 2.0, max_value= 17e6, value=6e5, step=1e4)
+att_dens = st.number_input('Population Density (per sq. mile)', min_value= 0, max_value= 12000, value=400, step=10)
+att_cost = st.number_input('Coastline/Area Ratio', min_value= 0, max_value= 800, value=30, step=10)
+att_migr = st.number_input('Annual Net Migration (migrant(s)/1,000 population)', min_value= -20, max_value= 25, value=0, step=2) 
+att_mort = st.number_input('Infant mortality (per 1000 births)', min_value= 0, max_value=195, value=40, step=10)
+att_litr = st.number_input('Population literacy Percentage', min_value= 0, max_value= 100, value=80, step=5)
+att_phon = st.number_input('Phones per 1000', min_value= 0, max_value= 1000, value=250, step=25)
+att_arab = st.number_input('Arable Land (%)', min_value= 0, max_value= 100, value=25, step=2)
+att_crop = st.number_input('Crops Land (%)', min_value= 0, max_value= 100, value=5, step=2)
+att_othr = st.number_input('Other Land (%)', min_value= 0, max_value= 100, value=70, step=2)
 st.text('(Arable, Crops, and Other land should add up to 100%)')
-att_clim = st.selectbox('Climate', options=(1, 1.5, 2, 2.5, 3))
+att_clim = st.select_slider('Climate', options=(1, 1.5, 2, 2.5, 3))
 st.write('''
          * 1: Mostly hot (like: Egypt and Australia)
          * 1.5: Mostly hot and Tropical (like: China and Cameroon)
@@ -40,13 +36,13 @@ st.write('''
          * 3: Mostly cold (like: Argentina and Belgium)
          '''
          )
-att_brth = st.slider('Annual Birth Rate (births/1,000)', min_value= 7, max_value= 50, value=20, step=2)
-att_deth = st.slider('Annual Death Rate (deaths/1,000)', min_value= 2, max_value= 30, value=10, step=2)
-att_agrc = st.slider('Agricultural Economy', min_value= 0.0, max_value= 1.0, value=0.15, step=0.05)
-att_inds = st.slider('Industrial Economy', min_value= 0.0, max_value= 1.0, value=0.25, step=0.05)
-att_serv = st.slider('Services Economy', min_value= 0.0, max_value= 1.0, value=0.60, step=0.05)
+att_brth = st.number_input('Annual Birth Rate (births/1,000)', min_value= 7, max_value= 50, value=20, step=2)
+att_deth = st.number_input('Annual Death Rate (deaths/1,000)', min_value= 2, max_value= 30, value=10, step=2)
+att_agrc = st.number_input('Agricultural Economy', min_value= 0.0, max_value= 1.0, value=0.15, step=0.05)
+att_inds = st.number_input('Industrial Economy', min_value= 0.0, max_value= 1.0, value=0.25, step=0.05)
+att_serv = st.number_input('Services Economy', min_value= 0.0, max_value= 1.0, value=0.60, step=0.05)
 st.text('(Agricultural, Industrial, and Services Economy should add up to 1)')
-att_regn = st.selectbox('Region', options=(1,2,3,4,5,6,7,8,9,10,11))
+att_regn = st.select_slider('Region', options=(1,2,3,4,5,6,7,8,9,10,11))
 st.write('''
          * 1: ASIA (EX. NEAR EAST)
          * 2: BALTICS
@@ -103,26 +99,25 @@ user_input = np.array([att_popl, att_area, att_dens, att_cost, att_migr,
                        att_regn_4, att_regn_5, att_regn_6, att_regn_7, 
                        att_regn_8, att_regn_9, att_regn_10, att_regn_11]).reshape(1,-1)
 
-#------
-# Model
-#------
 
-#import dataset
+
+#Dataset Import 
+
 def get_dataset():
-    data = pd.read_csv(f"{pathlib.Path().resolve()}/App/countries-of-the-world.csv")
+    data = pd.read_csv(f"{pathlib.Path().resolve()}/App/countries_gdp.csv")
     return data
 
 if st.button('Estimate GDP'):
     data = get_dataset()
     
-    #fix column names
+   #Renaming of Columns
     data.columns = (["country","region","population","area","density",
                      "coastline_area_ratio","net_migration","infant_mortality",
                      "gdp_per_capita","literacy","phones","arable","crops","other",
                      "climate","birthrate","deathrate","agriculture","industry",
                       "service"])
     
-    #Fix data types
+   #Defining of Data Types
     data.country = data.country.astype('category')
     data.region = data.region.astype('category')
     data.density = data.density.astype(str)
@@ -156,7 +151,7 @@ if st.button('Estimate GDP'):
     data.service = data.service.astype(str)
     data.service = data.service.str.replace(",",".").astype(float)
     
-    #fix missing data
+    #Filling Data
     data['net_migration'].fillna(0, inplace=True)
     data['infant_mortality'].fillna(0, inplace=True)
     data['gdp_per_capita'].fillna(2500, inplace=True)
@@ -172,24 +167,19 @@ if st.button('Estimate GDP'):
     data['service'].fillna(0.8, inplace=True)
     data['industry'].fillna((1 - data['agriculture'] - data['service']), inplace= True)
     
-    #Region Transform
     data_final = pd.concat([data,pd.get_dummies(data['region'], prefix='region')], axis=1).drop(['region'],axis=1)
     
-    #Data Split
+    #Training and Testing Data
     y = data_final['gdp_per_capita']
     X = data_final.drop(['gdp_per_capita','country'], axis=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
     
-    #model training
-    gbm_opt = GradientBoostingRegressor(learning_rate=0.01, n_estimators=500,
-                                        max_depth=5, min_samples_split=10, 
-                                        min_samples_leaf=1, subsample=0.7,
-                                        max_features=7, random_state=101)
-    gbm_opt.fit(X_train,y_train)
+    rdf = RandomForestRegressor(random_state=101, n_estimators=200)
+    rdf.fit(X_train,y_train)
     
-    #making a prediction
-    gbm_predictions = gbm_opt.predict(user_input) #user_input is taken from input attrebutes 
-    st.write('The estimated GDP per capita is: ', gbm_predictions)
+    #Predicting Data
+    rdf_predictions =  rdf.predict(user_input) #userInput
+    st.write('The estimated GDP per capita is: ',  rdf_predictions)
 
 
 
